f7158e24bb520ac9d68f5160ce0bed35c573bbca
Yoann Bettinelli <yoann.bettinelli@gmail.com>
2021-04-13 09:32:33 +0200

    fix count

--- traitement.php
+++ traitement.php
@@ -1,12 +1,14 @@
 <?php
 
-for ($i = 0; $i < count($_FILES['files']); $i++) {
-    $name =  $_FILES['files']['name'][$i];
+// var_dump($_FILES['files']);
+
+for ($i = 1; $i < count($_FILES['files']['name']); $i++) {
+    $name =  $_FILES['files']['name'][$i-1];
     $repertoire = "tmp_ged_files/ " . $name;
 
     move_uploaded_file($_FILES['files']['tmp_name'][$i], $repertoire);
 }
 
-echo "<pre>";
-var_dump($_FILES);
-echo "</pre>";
+// echo "<pre>";
+// var_dump($_FILES);
+// echo "</pre>";
